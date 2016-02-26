0418f67a1ade9567d229cd21d698a9d45f7ad247
FlyRoNik <hang.glider.viru007@gmail.com>
2016-02-26 21:01:23 +0200

    работает

---
+++ HelloWorld/src/ua/com/juja/core/HelloWorld.java
@@ -0,0 +1,10 @@
+package ua.com.juja.core;
+
+/**
+ * Created by FlyRoNik on 26.02.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
